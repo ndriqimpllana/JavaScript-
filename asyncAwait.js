4b4f1e286a37f81b0111b63b4cb8bc0e76e1749c
Ndricim Pllana <ndriqimpllana@gmail.com>
2025-07-03 13:07:02 -0400

    initial

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,56 +1,16 @@
-let beverages = ['tea', 'coffee', 'apple cider'];
 
-function pickRandomBeverage() {
- return new Promise(function(resolve, reject) {
-  let randomIndex = Math.floor(Math.random() * beverages.length);
-  let selectedBeverage = beverages[randomIndex];
-  setTimeout(function() {
-   console.log(`${selectedBeverage} selected`);
-   resolve(selectedBeverage);
-  }, 1000);
- });
-}
 
-function checkIfHotWaterIsReady(isBeverageSelected) {
- return new Promise(function(resolve, reject) {
-  setTimeout(function() {
-   if (isBeverageSelected) {
-    console.log("Preparing...");
-    resolve(isBeverageSelected);
-   } else {
-    reject("No beverage has been picked up.");
-   }
-  }, 1000);
- });
-}
 
-function prepareDrink(selectedBeverage) {
- return new Promise(function(resolve, reject) {
-  setTimeout(function() {
-   if (selectedBeverage) {
-    console.log(`Enjoy your ${selectedBeverage} !`);
-    resolve(true);
-   } else {
-    reject("Drink not ready yet...");
-   }
-  }, 1000);
- });
-}
 
-function nativePromiseHandling() {
- const pickedDrink = pickRandomBeverage();
- const handleWaterReadyCheck = function(selectedBeverage) {
-  return checkIfHotWaterIsReady(selectedBeverage);
- };
- const handlePrepareDrink = function(isBeverageSelected) {
-  return prepareDrink(isBeverageSelected);
- };
- pickedDrink
-  .then(
-handleWaterReadyCheck
-)
-  .then(
-handlePrepareDrink
-);
-}
-nativePromiseHandling();
+const student = {
+  name: 'Leila',
+  scores: [95, 87, 90],
+};
+
+// TODO:// - Destructure the name
+// - Destructure the first and third scores
+// - Log: "Leila scored 95 and 90"
+
+const { name, scores:[firstScore, ,thirdScore]} = student;
+
+console.log(`${name} scores are ${firstScore} and ${thirdScore}`);
